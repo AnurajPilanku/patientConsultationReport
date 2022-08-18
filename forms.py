9f8a7e1375bdd959c8f23558d910c5b5a66ca79b
Anuraj Pilanku <110219574+AnurajPilanku@users.noreply.github.com>
2022-08-18 17:07:53 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Consultation
-
-
-class ConsultationForm(ModelForm):
-
-    class Meta:
-        model = Consultation
-        fields = ['clinic_name', 'physician_name', 'description']
