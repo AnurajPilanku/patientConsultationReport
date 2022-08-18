fba6ac4d39be46ae04d37a087d88be9395661684
Anuraj Pilanku <110219574+AnurajPilanku@users.noreply.github.com>
2022-08-18 17:08:16 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Consultation
-
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Consultation, AuthorAdmin)
