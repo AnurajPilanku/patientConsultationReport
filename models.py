4c895a0c2a8dafdc85850600932c1bbb15f353cc
Anuraj Pilanku <110219574+AnurajPilanku@users.noreply.github.com>
2022-08-18 17:07:30 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Consultation(models.Model):
-    clinic_name = models.CharField(max_length=50, blank=True, null=True)
-    physician_name = models.CharField(max_length=50, blank=True, null=True)
-    description = models.TextField()
-    pdf_form = models.FileField(upload_to="generatedPdfs")
