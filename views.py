95b49dd4517f70ef95471c0305592a749c03bbd4
Anuraj Pilanku <110219574+AnurajPilanku@users.noreply.github.com>
2022-08-18 17:03:58 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,29 @@
+import requests
+
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from .generate_pdf import pdf_generator
+from .forms import ConsultationForm
+
+def consultation(request):
+    if request.method == "GET":
+        form = ConsultationForm()
+        context = {}
+        context["form"] = form
+        return render(request, "pages/index.html", context)
+    if request.method == "POST":
+        form = ConsultationForm(request.POST)
+        if form.is_valid():
+            new_consultation = form.save()
+            output_file_path, filename = pdf_generator(new_consultation)
+            new_consultation.pdf_form.name = output_file_path
+            new_consultation.save()
+            media_url = "/media/generatedPdfs/{}".format(filename)
+            return redirect(media_url)
+
+
+
+
+
+
